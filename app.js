27e95ffb15224f9ea0d404cdd900415938ec5075
uo258014 <47207829+uo258014@users.noreply.github.com>
2019-04-05 10:54:10 +0200

    Clase

--- app.js
+++ app.js
@@ -24,6 +24,15 @@ var mongo = require('mongodb');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+    // Debemos especificar todas las headers que se aceptan. Content-Type , token
+    next();
+});
+
 
 var gestorBD = require("./modules/gestorBD.js");
 gestorBD.init(app, mongo);
